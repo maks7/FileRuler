ad625e785e2f30b06d222ec476a03652f851226b
alexnad <alexandernadjarian@gmail.com>
2015-04-25 15:07:12 +0300

    add code for Word handling classes

--- FileRuler/src/word/to/tags/Word.java
+++ FileRuler/src/word/to/tags/Word.java
@@ -1,5 +1,50 @@
 package word.to.tags;
 
-public class Word {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
+public class Word {
+	private final String name;
+	private int wordOccurence;
+	private HashMap<String, TagInfo> tags = new HashMap<String, TagInfo>();
+	
+	public Word(String name){
+		this.name = name;
+	}
+	
+	public void addOcurrense(HashMap<String, Double> tags){
+		this.wordOccurence += 1;
+		Iterator<String> keys =  tags.keySet().iterator();
+		while(keys.hasNext()){
+			String tagName = keys.next();
+			this.addTag(tagName, tags.get(tagName));
+		}
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public HashMap<String, TagInfo> getInfo(){
+		return null;
+	}
+	
+	private double calculateRank(String tagName, double percentige){
+		double tagOccurence = tags.get(tagName).getOccurence();
+		double tagRank = tags.get(tagName).getRank();
+		
+		double offset =(double) (this.wordOccurence - tagOccurence);
+		double rank = (tagRank + 0.1*Math.pow(2, offset));
+		rank = (rank*tagOccurence + percentige) / this.wordOccurence;
+		return rank;
+	}
+	
+	private void addTag(String tagName, double percentige){
+		tags.get(tagName).addOcuurence();
+		double recalculated = calculateRank(tagName, percentige);
+		tags.get(tagName).setRank(recalculated);
+	}
 }
