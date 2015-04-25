9de336dffccb0debadc92b0ec19bf40320d19a7e
maks7 <maxim13bg@mail.bg>
2015-04-25 07:18:20 +0300

    updated FileRuler

---
+++ FileRuler.java
@@ -0,0 +1,83 @@
+package com.hackbulgaria.fileruler;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import javax.swing.JList;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import java.awt.Color;
+import javax.swing.DefaultComboBoxModel;
+
+public class FileRuler extends JFrame {
+
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    FileRuler frame = new FileRuler();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public FileRuler() {
+        setTitle("FileRuler");
+        setForeground(Color.YELLOW);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(200, 200, 650, 400);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        JLabel lblSearch = new JLabel("Enter name:");
+        lblSearch.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        lblSearch.setBounds(39, 29, 78, 14);
+        contentPane.add(lblSearch);
+        
+        JTextPane textPane = new JTextPane();
+        textPane.setForeground(Color.BLACK);
+        textPane.setBounds(39, 54, 456, 20);
+        contentPane.add(textPane);
+        
+        JButton btnNewButton = new JButton("Scan");
+        btnNewButton.setBounds(526, 11, 89, 23);
+        contentPane.add(btnNewButton);
+        
+        JButton btnNewButton_1 = new JButton("Find");
+        btnNewButton_1.setBounds(526, 51, 89, 23);
+        contentPane.add(btnNewButton_1);
+        
+        JList list = new JList();
+        list.setBounds(43, 113, 452, 219);
+        contentPane.add(list);
+        
+        JButton btnOpen = new JButton("Open");
+        btnOpen.setBounds(526, 110, 89, 23);
+        contentPane.add(btnOpen);
+        
+        JComboBox<String> comboBox = new JComboBox<>();
+        comboBox.setModel(new DefaultComboBoxModel(new String[] {"Films", "Images"}));
+        comboBox.setBounds(39, 85, 159, 20);
+        contentPane.add(comboBox);
+    }
+}
