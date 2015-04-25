f95c43f750612bc6fbcdb29f44ef06dc1aee2af5
zdgeorgiev <zddavko@gmail.com>
2015-04-25 07:10:18 +0300

    fixed json parse and added commons-io-2.4

--- FileRuler/src/com/hackbulgaria/fileruler/MovieFactory.java
+++ FileRuler/src/com/hackbulgaria/fileruler/MovieFactory.java
@@ -1,52 +1,58 @@
 package com.hackbulgaria.fileruler;
 
 import java.io.File;
-import java.io.FilePermission;
-import java.net.URI;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.io.IOException;
 
-import org.json.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MovieFactory extends Thread {
-	String actors;
-	String scenarist;
-	String director;
-	String runtime;
-	String ganre;
-	String yearOfRelease;
-	String name;
-	JSONObject movieContent;
-	String fileName;
+    String actors;
+    String scenarist;
+    String director;
+    String runtime;
+    String ganre;
+    String yearOfRelease;
+    String name;
+    JSONObject movieContent;
+    String fileName;
 
-	public MovieFactory(String filePathAndName) {
-		super(filePathAndName);
-	}
+    public MovieFactory(String filePathAndName) {
+        super(filePathAndName);
+    }
 
-	@Override
-	public void run() {
-		// String fileName = new
-		// File(Paths.get(URI.create(getName()))).getName();
-		fileName = new File(getName()).getName();
-		fileName = fileName.substring(0, fileName.lastIndexOf('.'));
-		movieContent = MovieUtils.getMovieContent(fileName);
-		name = movieContent.getString("Title");
-		yearOfRelease = movieContent.getString("Year");
-		actors = movieContent.getString("Actors");
-		scenarist = movieContent.getString("Writer");
-		director = movieContent.getString("Director");
-		runtime = movieContent.getString("Runtime");
-		ganre = movieContent.getString("Genre");
-		MoviesCollecion.movieCollection.add(new Movie(name, yearOfRelease,
-				actors, scenarist, director, runtime, ganre));
+    @Override
+    public void run() {
+        fileName = new File(getName()).getName();
+        fileName = fileName.substring(0, fileName.lastIndexOf('.'));
 
-	}
+        try {
+            movieContent = new JSONObject(MovieUtils.getMovieContent(fileName));
+            name = movieContent.getString("Title");
+            yearOfRelease = movieContent.getString("Year");
+            actors = movieContent.getString("Actors");
+            scenarist = movieContent.getString("Writer");
+            director = movieContent.getString("Director");
+            runtime = movieContent.getString("Runtime");
+            ganre = movieContent.getString("Genre");
+        } catch (JSONException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
 
-	void generateAllMovies() {
-		for (String pathToFile : HDDCrawler.listOfMovies) {
-			new MovieFactory(pathToFile).start();
+        MoviesCollecion.movieCollection
+                .add(new Movie(name, yearOfRelease, actors, scenarist, director, runtime, ganre));
 
-		}
-	}
+    }
+
+    void generateAllMovies() {
+        for (String pathToFile : HDDCrawler.listOfMovies) {
+            new MovieFactory(pathToFile).start();
+
+        }
+    }
 
 }
